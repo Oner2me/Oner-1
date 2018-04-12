90b4e2e518b4ff5524adccf010ddd100045aec5e
oner2me <947459097@qq.com>
2018-04-12 17:18:41 +0800

    存里一些软件和数据 ---继续更改页面

--- practice.js
+++ practice.js
@@ -1,3 +1,9 @@
-//var abc;
-//def=abc;
-//console.log(def);
+
+$(function(){
+    $(document).bind('mousemove',function(e){
+        $('#text').text('x轴坐标：'+ e.pageX+','+'Y轴坐标'+ e.pageY);
+    });
+    $('.a1').click(function(e){
+        e.preventDefault();
+    });
+});
