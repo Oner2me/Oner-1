48b2619156fc11e1c66511aa05d37243a9bcce82
oner2me <947459097@qq.com>
2018-05-13 13:44:56 +0800

    购物车已完成----

--- MallStation/js/shop_cart.js
+++ MallStation/js/shop_cart.js
@@ -2,13 +2,16 @@
 $('.col_width_1 div').click(function(){
     $(this).find('.mark').toggle();
 });
+
 //页面加载时调用计价系统
 pricing();
+
 //当勾选商品是调用一次计价系统
 $('tbody .col_width_1 div').click(function(){
     Allselect();
     pricing();
 });
+
 //当勾选全选时，让所有商品都选上
 $('thead .col_width_1 div').click(function(){
     var state = $(this).find('.mark').is(':hidden');
@@ -19,12 +22,37 @@ $('thead .col_width_1 div').click(function(){
     }
     pricing();
 });
-
+//添加商品数量
+$('tbody .col_width_7 div .add').click(function(){
+    var n=parseInt($(this).siblings('input').val());
+    $(this).siblings('input').val(++n);
+    pricing();
+});
+//减少商品数量
+$('tbody .col_width_7 div .minus').click(function(){
+   var n=parseInt($(this).siblings('input').val());
+    --n;
+    if(n<1){
+        n=1;
+    }
+    $(this).siblings('input').val(n);
+});
+//手动改变input框中的数据
+$('tbody .col_width_7 div input').change(function(){
+    var value = $(this).val();
+    var re=/^[0-9]*$/;
+    if(!(re.test($(this).val()))||$(this).val()<1){
+        $(this).val(1);
+    }
+    pricing();
+});
 //删除商品
 $('tbody .col_width_9 span').click(function(){
     if(confirm('您确定要删除么？')){
-        $(this).parent('tr').remove();
+        $(this).parents('tr').remove();
         Allselect();
+    }else{
+        return false;
     }
     pricing();
 });
@@ -53,9 +81,36 @@ function pricing(){
 function Allselect(){
     var len = $('table tbody tr').length;
     var show = $('tbody tr').find('.mark').filter(':visible').length;
-    if(len == show){
+    if(len == show && len != 0){
         $('thead .col_width_1 div .mark').css('display','block');
     }else{
         $('thead .col_width_1 div .mark').css('display','none');
     }
 }
+
+//清空购物车
+$('section .settlement ul li:first-child span').click(function(){
+    if(confirm('您确定删除所有商品么？')){
+        $('tbody tr').remove();
+        $('thead .col_width_1 div .mark').css('display','none');
+        $('section .settlement ul .payable b').text('0');
+        $('section .settlement ul .totals b').text('0');
+    }else{
+        return false;
+    }
+});
+
+//去结算
+$('section .settlement a').click(function(){
+    var len = $('table tbody tr').length;
+    var show = $('tbody tr').find('.mark').filter(':visible').length;
+    if(len > 0 && show >0){
+        return true;
+    }else if(len > 0 && show == 0){
+        alert('您的商品没有被选中，请勾选商品');
+        return false;
+    }else if(len==0){
+        alert('您还没有商品，请去选购');
+        return false;
+    }
+});
