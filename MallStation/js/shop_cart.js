ecd76e5f250bbf963623f1792107aa5984a05de7
oner2me <947459097@qq.com>
2018-05-13 12:43:42 +0800

    购物车暂存----

--- MallStation/js/shop_cart.js
+++ MallStation/js/shop_cart.js
@@ -2,14 +2,33 @@
 $('.col_width_1 div').click(function(){
     $(this).find('.mark').toggle();
 });
+//页面加载时调用计价系统
 pricing();
+//当勾选商品是调用一次计价系统
+$('tbody .col_width_1 div').click(function(){
+    Allselect();
+    pricing();
+});
+//当勾选全选时，让所有商品都选上
+$('thead .col_width_1 div').click(function(){
+    var state = $(this).find('.mark').is(':hidden');
+    if(state){
+        $('tbody .col_width_1 div .mark').css('display','none');
+    }else{
+        $('tbody .col_width_1 div .mark').css('display','block');
+    }
+    pricing();
+});
 
+//删除商品
+$('tbody .col_width_9 span').click(function(){
+    if(confirm('您确定要删除么？')){
+        $(this).parent('tr').remove();
+        Allselect();
+    }
+    pricing();
+});
 
-//订单总额
-var totals = $('.settlement ul .totals b');
-//本次应付
-var payable = $('.settlement ul .payable');
-console.log();
 //计价系统 pricing system
 function pricing(){
     //单价
@@ -18,17 +37,25 @@ function pricing(){
     var num = $('tbody .col_width_7 div input');
     //总价
     var sum = 0;
-    //总数
-    var count = 0;
     var total = [];
     $('tbody .col_width_1 div .mark').each(function(i){
         total[i]=(price[i].innerHTML*num[i].value).toFixed(2);
         if($(this).is(":visible")){
             sum+=parseFloat(total[i]);
-            count+=parseFloat(num[i]);
         }
     });
-    console.log(sum);
-    console.log(count);
-    //$("#id").is(":hidden")
+    //将总价插入到HTML页面中
+    $('section .settlement ul .payable b').text(sum);
+    $('section .settlement ul .totals b').text(sum);
+}
+
+//判断是否勾选全选
+function Allselect(){
+    var len = $('table tbody tr').length;
+    var show = $('tbody tr').find('.mark').filter(':visible').length;
+    if(len == show){
+        $('thead .col_width_1 div .mark').css('display','block');
+    }else{
+        $('thead .col_width_1 div .mark').css('display','none');
+    }
 }
